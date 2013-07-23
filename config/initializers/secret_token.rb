ffee75de77cf3f2f7a3d1c2b9d3de7476eac75a3
@kae_kasui <kaekasui_github@icloud.com>
2013-07-24 08:50:05 +0900

    refs #141 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Room::Application.config.secret_key_base = '0eff09adfdb742924bccd9aa894a29d3a71b83bdb8f0a93e63f36139003c74c6d778280973894d91c081bd301857c2e35f7535315397490272890db92c9a619a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Room::Application.config.session_store :cookie_store, key: '_room_session'
