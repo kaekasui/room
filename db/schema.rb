30be875d4e5d8820391343fb5fe883cc154dcf0f
@kae_kasui <kaekasui_github@icloud.com>
2013-08-27 18:37:08 +0900

    refs #221 'rake db:migrate' & 'rake db:migrate RAILS_ENV=test'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130826061219) do
+ActiveRecord::Schema.define(version: 20130827093450) do
 
   create_table "blog_comments", force: true do |t|
     t.integer  "blog_id"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 20130826061219) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_id"
+    t.string   "type"
   end
 
 end
