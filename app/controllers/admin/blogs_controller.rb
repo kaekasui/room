9e8af5363c6e883a4f0846524f89e8a6d50e1a57
@kae_kasui <kaekasui_github@icloud.com>
2013-08-28 14:57:18 +0900

    refs #181 update blogs on the admin page

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -1,7 +1,15 @@
 class Admin::BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update]
 
   def index
     @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,11 +25,23 @@ class Admin::BlogsController < ApplicationController
     end
   end
 
-  def new
-    @blog = Blog.new
+  def update
+    respond_to do |format|
+      if @blog.update_attributes(blog_params)
+        format.html { redirect_to admin_blogs_path, notice: I18n.t("flash.update_blog") }
+        format.json { render json: @blog, status: :updated, location: @blog }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
 
   def blog_params
     params.require(:blog).permit(:title, :contents1, :contents2)
