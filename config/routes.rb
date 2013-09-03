c1b825df456bd7492b6348ca3fffef7e090dfc15
@kae_kasui <kaekasui_github@icloud.com>
2013-09-03 09:24:38 +0900

    refs #241 set the 'admin menu' when you login as the administrator

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Room::Application.routes.draw do
 
-  root 'home#index'
+  root "home#index"
   get "/auth/:provider/callback" => "sessions#callback"
   get "/logout" => "sessions#destroy", as: "logout"
 
   resources :blogs, only: [:index, :show] do
     resources :blog_comments
   end
-  resources :blog_links
   resources :mail_forms, only: [:create]
 
   namespace :admin do
