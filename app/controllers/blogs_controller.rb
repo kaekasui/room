71cba030c3444c2af38a95caf7e5bd2ddfa4527b
@kae_kasui <kaekasui_github@icloud.com>
2013-09-10 12:04:04 +0900

    refs #181 delete method of an unused parameter

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,8 +22,4 @@ class BlogsController < ApplicationController
     @recent_blog_comments = BlogComment.recent_blog_comments
     @blog_links = BlogLink.all
   end
-
-  def blog_params
-    params.require(:blog).permit(:title, :blog_category_code, :contents1, :contents2, :draft, :deleted_at)
-  end
 end
