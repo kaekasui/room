006f4bf30be8f40acbc13bfdb4f08db6bc7731cf
@kae_kasui <kaekasui_github@icloud.com>
2013-09-25 20:41:38 +0900

    refs #281 add function to create and edit versions on the 'index'

--- app/assets/javascripts/admin/versions.js.coffee
+++ app/assets/javascripts/admin/versions.js.coffee
@@ -1,3 +1,14 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$ ->
+  add_count = 0
+  $("td.add_version").click ->
+    add_count += 1
+    $("table.table").append(
+      "<tr>
+      <td><input type='radio' name='version[current]' value='new_#{add_count}'></td>
+      <td><input type='text' name='version[new][#{add_count}]'></td>
+      </tr>"
+    )
--- app/controllers/admin/versions_controller.rb
+++ app/controllers/admin/versions_controller.rb
@@ -5,11 +5,22 @@ class Admin::VersionsController < ApplicationController
   end
 
   def update_all
+    # 既存バージョン情報の更新
     @versions.each do |version|
       version_param = version_params[version.id.to_s]
       version.update_attributes(version_param.symbolize_keys)
-      version.id == current_version_params.to_i ? version.update_attributes(current: true) : version.update_attributes(current:false)
+      version.id == current_version_params.to_i ? version.update_attributes(current: true) : version.update_attributes(current: false)
     end
+
+    # 新規バージョン情報の作成
+    version_params["new"].each do |param_id, value|
+      version = Version.create(name: value)
+      if current_version_params.to_i == 0
+        current_id = current_version_params.slice(4..-1)
+        param_id == current_id ? version.update_attributes(current:true) : version.update_attributes(current: false)
+      end
+    end
+
     redirect_to admin_versions_path
   end
 
@@ -19,7 +30,7 @@ class Admin::VersionsController < ApplicationController
   end
 
   def current_version_params
-    params.require(:current).permit(:current_version)[:current_version]
+    params.require(:version).permit(:current)[:current]
   end
 
   def find_versions
