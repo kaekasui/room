0fdfb9d19332ca437c0318f4c7f98bbf50c590c6
@kae_kasui <kaekasui_github@icloud.com>
2013-10-02 23:52:13 +0900

    refs #291 add validationfor blog comments

--- app/models/blog_comment.rb
+++ app/models/blog_comment.rb
@@ -7,7 +7,10 @@ class BlogComment < ActiveRecord::Base
   scope :recent_blog_comments, -> { limit(5) }
   scope :with_no_draft, -> { where(draft: false) }
 
-  validates :author, :content, presence: true
+  validates :title, :password, length: {maximum: MAX_TEXT_FIELD_LENGTH }
+  validates :url, :mail, length: { maximum: MAX_LONG_TEXT_FIELD_LENGTH }
+  validates :author, presence: true, length: { maximum: MAX_TEXT_FIELD_LENGTH }
+  validates :content, presence: true, length: { maximum: MAX_TEXT_AREA_LENGTH }
 
   def blog_title
     Blog.find(self.blog_id).title
