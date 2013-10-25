efe0b68982f4c0733b1b96f0cd4f8e531daf5c55
@kae_kasui <kaekasui_github@icloud.com>
2013-10-25 12:03:09 +0900

    refs #2 delete 'code' from tickets

--- db/migrate/20131022045721_create_tickets.rb
+++ db/migrate/20131022045721_create_tickets.rb
@@ -1,10 +1,9 @@
 class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
-      t.integer :code
       t.string :title
       t.text :content
-      t.integer :progress
+      t.integer :progress, default: 0
       t.integer :status_id
       t.integer :priority_id
       t.integer :version_id
