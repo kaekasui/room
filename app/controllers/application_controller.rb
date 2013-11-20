fdc481f8f4f233aed28e1bb8ba6651da614a8744
@kae_kasui <kaekasui_github@icloud.com>
2013-11-20 12:47:02 +0900

    refs #21 sign up, sign in and sign out with 'devise'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def current_user
-    @current_user = User.where(id: session[:user_id]).first
-  end
-
-  helper_method :current_user
 end
