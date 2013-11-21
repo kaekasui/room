0d02ca74c7369dc63fcad20b86495db5038eb0e2
@kae_kasui <kaekasui_github@icloud.com>
2013-11-22 02:05:26 +0900

    refs #31 generate random access code of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :set_access_code
   acts_as_paranoid
+
+  private
+
+  def set_access_code
+    self.access_code = self.access_code.blank? ? generate_access_code : self.access_code
+  end
+
+  def generate_access_code
+    code = SecureRandom.urlsafe_base64(6)
+    self.class.where(access_code: code).blank? ? code : generate_access_code
+  end
 end
