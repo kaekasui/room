04a7b008cb3ef94a308e8f5274a61922139082eb
@kae_kasui <kaekasui_github@icloud.com>
2013-11-26 08:53:31 +0900

    refs #21 display sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Room::Application.routes.draw do
 
-  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
+  devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks'}
 
   root "home#index"
 
