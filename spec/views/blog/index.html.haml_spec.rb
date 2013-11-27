0b4b7b4256cf079246dac9c75c94cf7e51d32fa7
@kae_kasui <kaekasui_github@icloud.com>
2013-11-27 18:37:14 +0900

    refs #30 add spec of blogs view

--- spec/views/blog/index.html.haml_spec.rb
+++ spec/views/blog/index.html.haml_spec.rb
@@ -1,79 +1,81 @@
 require 'spec_helper'
 
 describe "blogs/index" do
-=begin
-  describe "ブログの記事" do
+  describe "display blogs list" do
     before do
-      3.times { FactoryGirl.create(:blog) }
       @blogs = Blog.with_no_draft.page(params[:page]).per(10)
       @recent_blogs = Blog.with_no_draft.recent_blogs
       @recent_blog_comments = BlogComment.recent_blog_comments
       @blog_links = BlogLink.all
     end
 
-    context "3件登録されている場合" do
-      it "一覧に3件表示されること" do
+    context "with 3 blogs." do
+      before do
+        3.times { create(:blog_example) }
+      end
+
+      it "display 3 blogs on the list." do
         render
         expect(rendered).to have_selector("table.table > tr > th.post_at", count: 3)
       end
-  
-      it "「最近の記事」に3件表示されること" do
+
+      it "display 3 blogs on the side menu." do
         render
+        expect(view.content_for(:sidebar)).to have_selector("ul.nav > li > a.recent_blog", count: 3)
+      end
+
+      it "don't display the draft blogs." do
+        create(:blog_example, draft: true)
+        render
+        expect(rendered).to have_selector("table.table > tr > th.post_at", count: 3)
         expect(view.content_for(:sidebar)).to have_selector("ul.nav > li > a.recent_blog", count: 3)
       end
     end
 
-    context "6件登録されている場合" do
+    context "with 6 blogs." do
       before do
-        3.times { FactoryGirl.create(:blog) }
+        6.times { create(:blog_example) }
+        render
       end
 
-      it "一覧に6件表示されること" do
-        render
+      it "display 6 blogs on the list." do
         expect(rendered).to have_selector("table.table > tr > th.post_at", count: 6)
       end
 
-      it "「最近の記事」に5件表示されること" do
-        render
+      it "display 5 blogs on the side menu." do
         expect(view.content_for(:sidebar)).to have_selector("ul.nav > li > a.recent_blog", count: 5)
       end
     end
 
-    context "12件登録されている場合" do
+    context "with 12 blogs" do
       before do
-        8.times { FactoryGirl.create(:blog) }
-        FactoryGirl.create(:latest_blog)
+        12.times { create(:blog_example) }
+        render
+      end
+      context "on the first page." do
+        it "display 10 blogs on the list." do
+          expect(rendered).to have_selector("table.table > tr > th.post_at", count: 10)
+        end
+
+        it "display 5 blogs on the side menu." do
+          expect(view.content_for(:sidebar)).to have_selector("ul.nav > li > a.recent_blog", count: 5)
+        end
       end
 
-      it "一覧に10件表示されること" do
-        render
-        expect(rendered).to have_selector("table.table > tr > th.post_at", count: 10)
-      end
+      context "on the second page." do
+        it "display 2 blogs on the list." do
+          @blogs = Blog.with_no_draft.page(2).per(10)
+	  expect(@blogs.count).to eq 2
+        end
 
-      it "一覧の2ページ目に2件表示されること" do
-        @blogs = Blog.with_no_draft.page(2).per(10)
-        render
-        expect(rendered).to have_selector("table.table > tr > th.post_at", count: 2)
-      end
-
-      it "「最近の記事」に5件表示されること" do
-        render
-        expect(view.content_for(:sidebar)).to have_selector("ul.nav > li > a.recent_blog", count: 5)
-      end
-
-      it "「最近の記事」に最新の記事が表示されること" do
-        render
-        expect(view.content_for(:sidebar)).to have_selector("ul.nav > li > a.recent_blog", content: "Title2")
-      end
-
-      it "「最近の記事」に下書きの記事が表示されないこと" do
-        FactoryGirl.create(:draft_blog)
-        render
-        expect(view.content_for(:sidebar)).to_not have_selector("ul.nav > li > a.recent_blog", content: "Title3")
+        it "display 5 blogs on the side menu." do
+          expect(view.content_for(:sidebar)).to have_selector("ul.nav > li > a.recent_blog", count: 5)
+        end
       end
     end
   end
- 
+
+=begin
   describe "サイドメニュー" do
     before do
       3.times { FactoryGirl.create(:blog) }
