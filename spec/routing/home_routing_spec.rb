774203d3866d832d14f3df7caa70a9752f5f2fd8
@kae_kasui <kaekasui_github@icloud.com>
2013-11-28 13:01:06 +0900

    refs #37 add spec of home and blog routing

--- spec/routing/home_routing_spec.rb
+++ spec/routing/home_routing_spec.rb
@@ -2,8 +2,7 @@ require "spec_helper"
 
 describe "HOME" do
   describe "routing" do
-
-    it "トップページが表示されること" do
+    it "display the top page." do
       expect(get("/")).to route_to("home#index")
     end
   end
