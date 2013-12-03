7ce98276c4ad25dc7415fdf82003c71c3b7a3d1a
@kae_kasui <kaekasui_github@icloud.com>
2013-12-03 23:23:44 +0900

    refs #28 add spec of admin/blogs_controller.rb

--- spec/controllers/admin/blogs_controller_spec.rb
+++ spec/controllers/admin/blogs_controller_spec.rb
@@ -87,7 +87,7 @@ describe Admin::BlogsController do
         end
 
 	it "is draft." do
-          pending # 下書きであること
+          expect((assigns[:blog]).draft).to eq true
 	end
       end
 
@@ -117,6 +117,111 @@ describe Admin::BlogsController do
     it "access :edit" do
       get :edit, {id: blog.id}
       expect(response).to render_template(:edit)
+    end
+  end
+
+  describe "#update" do
+    context "when update a blog." do
+      let(:blog) { create(:blog_example) }
+
+      context "with full data." do
+        before do
+          patch :update, { blog: { title: "Title", contents1: "Contents1", contents2: "Contents2" }, id: blog.id, commit: I18n.t("actions.update") }
+	end
+
+        it "update a record." do
+          expect(assigns[:blog]).to eq blog
+        end
+
+        it "display messages." do
+          expect(flash[:notice]).to eq I18n.t("flash.update_blog")
+        end
+
+	it "redirect to :index" do
+          expect(response).to redirect_to admin_blogs_path
+        end
+
+	it "is not draft." do
+          expect((assigns[:blog]).draft).to eq false
+	end
+      end
+
+      context "with blank title." do
+        before do
+          patch :update, { blog: { title: "", contents1: "Contents1", contents2: "Contents2" }, id: blog.id, commit: I18n.t("actions.draft") }
+	end
+
+        it "raise errors." do
+          expect((assigns[:blog]).errors.count).to eq 1
+        end
+
+	it "display message." do
+          pending
+	end
+
+	it "redirect to :new" do
+          pending
+	end
+      end
+    end
+    
+    context "when update a draft blog." do
+      let(:blog) { create(:blog_example, draft: true) }
+
+      context "with full data." do
+        before do
+          patch :update, { blog: { title: "Title", contents1: "Contents1", contents2: "Contents2" }, id: blog.id, commit: I18n.t("actions.draft") }
+	end
+
+        it "update a record." do
+          expect(assigns[:blog]).to eq blog
+        end
+
+        it "display messages." do
+          expect(flash[:notice]).to eq I18n.t("flash.update_blog")
+        end
+
+	it "redirect to :index" do
+          expect(response).to redirect_to admin_blogs_path
+        end
+
+	it "is draft." do
+          expect((assigns[:blog]).draft).to eq true
+	end
+      end
+
+      context "with blank title." do
+        before do
+          patch :update, { blog: { title: "", contents1: "Contents1", contents2: "Contents2" }, id: blog.id, commit: I18n.t("actions.draft") }
+	end
+
+        it "raise error." do
+          expect((assigns[:blog]).errors.count).to eq 1
+        end
+
+	it "display message." do
+          pending
+	end
+
+	it "redirect to :new" do
+          pending
+	end
+      end
+    end
+  end
+
+  describe "#destroy" do
+    let(:blog) { create(:blog_example) }
+
+    it "is success." do
+      delete :destroy, { id: blog.id }
+      expect(response.status).to eq 302
+      # TODO:正しいステータスコードであるかを確認する必要あり
+    end
+
+    it "update the 'deleted_at'." do
+      delete :destroy, { id: blog.id }
+      expect((assigns[:blog]).deleted_at).not_to be_nil
     end
   end
 end
