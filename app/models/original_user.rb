4bd4ae268043b68d8e4d244e532738a2834dda4f
@kae_kasui <kaekasui_github@icloud.com>
2013-12-17 17:32:26 +0900

    refs #47 modified original user model

--- app/models/original_user.rb
+++ app/models/original_user.rb
@@ -2,7 +2,6 @@ class OriginalUser < User
   validates :email, uniqueness: true,
 		    length: { maximum: MAX_LONG_TEXT_FIELD_LENGTH },
 		    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :password, presence: true,
-	               confirmation: :password_confirmation,
+  validates :password, confirmation: :password_confirmation,
 		       length: { within: 6..MAX_TEXT_FIELD_LENGTH }
 end
