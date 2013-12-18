f616f46ffbbbd20be1d2e58b809dd23046d7ab55
@kae_kasui <kaekasui_github@icloud.com>
2013-12-18 12:27:39 +0900

    refs #31 add spec of user login

---
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe Users::SessionsController do
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
+  describe "#new" do
+    context "with non-logged-in user" do
+      it "is success." do
+        get :new
+        expect(response.status).to eq 200
+      end
+
+      it "access :new" do
+        get :new
+        expect(response).to render_template(:new)
+      end
+    end
+
+    context "with logged-in user" do
+      before do
+        controller.stub(:authenticate_user!).and_return true
+	user = create(:original_user_example)
+	sign_in user
+      end
+
+      it "is success." do
+        get :new
+        expect(response.status).to eq 302
+      end
+
+      it "access :root" do
+        get :new
+        expect(response).to redirect_to(root_path)
+      end
+   end
+
+   after do
+     sign_out :user
+   end
+  end
+
+  describe "#create" do
+    let(:user) { create(:original_user) }
+
+    context "when data is normal." do
+      it "login." do
+        post :create, { user: {email: "abc@example.com", password: "password" }}
+        expect(response.status).to eq 200
+      end
+    end
+
+    context "when email is blank." do
+      it "not login." do
+        post :create, { user: {email: "", password: "password" }}
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+end
