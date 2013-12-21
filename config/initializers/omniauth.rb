1a70e3664c4ac868e329a82a7ade55979e3f4c61
@kae_kasui <kaekasui_github@icloud.com>
2013-12-21 18:13:23 +0900

    refs #48 remove setting

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :dropbox, ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET']
+  #provider :dropbox, ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET']
 end
