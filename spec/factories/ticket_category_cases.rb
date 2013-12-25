6dfa677166b7fc96e4ae4f05bcc2839731df3599
@kae_kasui <kaekasui_github@icloud.com>
2013-12-25 12:10:53 +0900

    refs #76 delete spec files

--- spec/factories/ticket_category_cases.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :ticket_category_case do
-    ticket_id 1
-    ticket_category_id 1
-  end
-end
