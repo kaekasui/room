0642ac55988a2440d9353074f1c4a9bc216ac61d
@kae_kasui <kaekasui_github@icloud.com>
2013-12-25 13:27:11 +0900

    refs #79 add project model spec, and modified to rein in the number of letters

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,9 +1,6 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
-  factory :project do
+  factory :project_example, class: Project do
     title "MyString"
     content "MyText"
-    deleted_at "2013-10-22 00:28:22"
   end
 end
