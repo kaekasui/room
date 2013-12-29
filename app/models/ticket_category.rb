ab194f1ae7012ab8f835e1d3deed1e9a1fec97c6
@kae_kasui <kaekasui_github@icloud.com>
2013-12-29 13:26:40 +0900

    refs #96 add relationship of project and ticket category, add spec

--- app/models/ticket_category.rb
+++ app/models/ticket_category.rb
@@ -1,3 +1,8 @@
 class TicketCategory < ActiveRecord::Base
+  acts_as_paranoid
+
+  belongs_to :project
   has_many :ticket_category_cases
+
+  validates :name, presence: true, length: { maximum: MAX_TEXT_FIELD_LENGTH }
 end
