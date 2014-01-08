d6314a41e1d1d8ea6683ded01850ca62639a507e
@kae_kasui <kaekasui_github@icloud.com>
2014-01-08 11:14:32 +0900

    refs #17 add relationship of models, and add the specs

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   acts_as_paranoid
   validates :name, presence: true, length: { maximum: MAX_TEXT_FIELD_LENGTH }
   belongs_to :project
-  has_many :categorizations
+  has_many :categorizations, dependent: :destroy
   has_many :tickets, through: :categorizations
   has_many :blogs, through: :categorizations
 end
